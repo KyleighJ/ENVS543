71533fc98715c7d9a54bbb38c2ffa9f9f79873ef
Your Name <you@example.com>
2024-09-26 16:32:41 -0400

    rice

--- getRiceData.R
+++ getRiceData.R
@@ -26,17 +26,27 @@ names(rice)
 
 rice |>
   mutate( Date = mdy_hms(DateTime, tz="EST") ) |>
-  mutate(Weekday = wday( Date, 
+  mutate( Weekday = wday( Date, 
                          label = TRUE,
-                         abbr = FALSE) ) |>
-  group_by( Weekday ) |>
+                         abbr = FALSE),
+          Month = month( Date, 
+                        label = TRUE,
+                        abbr = FALSE ) )|>
+  group_by( Month, Weekday ) |>
   mutate( H2O_Temp = 1.8 * H2O_TempC + 32 ) |>
   select( -H2O_TempC ) |>
   select( DateTime, H2O_Temp, ODO_sat, Depth_ft, Turbidity_ntu, PH, Salinity_ppt,
           AirTempF, Rain_in, WindSpeed_mph, WindDir ) |>
   summarize( `Air Temp`= mean( AirTempF ),
+             `Wind Speed` = mean( WindSpeed_mph),
+             `Wind Direction` = mean( WindDir ),
              `Rain Inches` = mean(Rain_in),
-             `Water Temp` = mean( H2O_Temp, na.rm = TRUE))|>
+             `Depth` = mean( Depth_ft ),
+             `Water Temp` = mean( H2O_Temp, na.rm = TRUE),
+             `ODO` = mean( ODO_sat, na.rm = TRUE ),
+             `Turbidity` = mean( Turbidity_ntu, na.rm = TRUE ),
+             `Salinity` = mean( Salinity_ppt, na.rm = TRUE ),
+             `pH` = mean( PH, na.rm = TRUE ))|>
   kable()|>
   kable_minimal()
 
