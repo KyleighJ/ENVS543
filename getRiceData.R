e48e8d949327c75e11587fd5f7bd60104a5897d3
Your Name <you@example.com>
2024-09-27 13:55:36 -0400

    get rice data function

--- getRiceData.R
+++ getRiceData.R
@@ -1,52 +1,32 @@
 
 getRiceData <- function(){
   
+  library(tidyverse)
+  library(lubridate)
   
+  url <- "https://docs.google.com/spreadsheets/d/1Mk1YGH9LqjF7drJE-td1G_JkdADOU0eMlrP01WFBT8s/pub?gid=0&single=true&output=csv"
+  
+  read_csv( url )  |>
+#turning date data into Date object
+    mutate( Date = mdy_hms(DateTime, tz="EST") ) |>
+#creating weekday, month, and day object
+    mutate( Weekday = wday( Date, 
+                            label = TRUE,
+                            abbr = FALSE),
+            Month = month( Date, 
+                           label = TRUE,
+                           abbr = FALSE ),
+            Day = day( Date ))|>
+#converting F to C
+    mutate( AirTempC = 1.8 * (AirTempF - 32) ) |>
+    mutate( Depth_m = Depth_ft / 3.281) |>
+    select( Date, Month, Day, Weekday, 
+            H2O_TempC, ODO_sat, Depth_m, Turbidity_ntu, PH, Salinity_ppt,
+            AirTempC, Rain_in, WindSpeed_mph, WindDir ) -> RICE
+
+  return(RICE)
 }
 
 
-library(tidyverse)
-library(lubridate)
-library(knitr)
-library(kableExtra)
 
-url <- "https://docs.google.com/spreadsheets/d/1Mk1YGH9LqjF7drJE-td1G_JkdADOU0eMlrP01WFBT8s/pub?gid=0&single=true&output=csv"
-
-read_csv( url ) -> rice
-
-# make date object
-# make month and weekday object
-# convert F -> C
-# get rid of extra data
-# reorder the columns
-
-# return( rice )
-
-names(rice)
-
-rice |>
-  mutate( Date = mdy_hms(DateTime, tz="EST") ) |>
-  mutate( Weekday = wday( Date, 
-                         label = TRUE,
-                         abbr = FALSE),
-          Month = month( Date, 
-                        label = TRUE,
-                        abbr = FALSE ) )|>
-  group_by( Month, Weekday ) |>
-  mutate( H2O_Temp = 1.8 * H2O_TempC + 32 ) |>
-  select( -H2O_TempC ) |>
-  select( DateTime, H2O_Temp, ODO_sat, Depth_ft, Turbidity_ntu, PH, Salinity_ppt,
-          AirTempF, Rain_in, WindSpeed_mph, WindDir ) |>
-  summarize( `Air Temp`= mean( AirTempF ),
-             `Wind Speed` = mean( WindSpeed_mph),
-             `Wind Direction` = mean( WindDir ),
-             `Rain Inches` = mean(Rain_in),
-             `Depth` = mean( Depth_ft ),
-             `Water Temp` = mean( H2O_Temp, na.rm = TRUE),
-             `ODO` = mean( ODO_sat, na.rm = TRUE ),
-             `Turbidity` = mean( Turbidity_ntu, na.rm = TRUE ),
-             `Salinity` = mean( Salinity_ppt, na.rm = TRUE ),
-             `pH` = mean( PH, na.rm = TRUE ))|>
-  kable()|>
-  kable_minimal()
 
